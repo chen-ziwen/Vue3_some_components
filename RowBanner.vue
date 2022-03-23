cb8a371e8ea63304ef8607c6b52cb2e944e1df0c
Chiko <2452559902@qq.com>
2022-03-23 09:32:23 +0800

    Add files via upload
    换肤分隔小组件

---
+++ RowBanner.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="row-banner">
+     <div class="border style flex">
+         <slot></slot>
+     </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+ import { computed } from 'vue';
+ interface RowBanner {
+     theme : string
+ }
+ const props = defineProps<RowBanner>()
+ const  src = computed(()=>`url(/assets/images/common/RowBanner/${props.theme}.png)`)
+</script>
+
+<style lang="scss" scoped>
+.row-banner {
+ display: inline-block;
+.border {
+ border-image-source: v-bind(src);
+ border-image-slice:0 35 0 35 fill;
+ border-image-width:0px 35px 0px 35px;
+ border-image-outset:0px 35px 0px 35px;
+ border-image-repeat:round;
+}
+.style {
+ display: inline-block;
+ white-space: nowrap;
+ width: auto;
+ height: 80px;
+ font-size: 24px;
+}
+.flex {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+}
+</style>
---
+++ RowBannerLi.vue
@@ -0,0 +1,24 @@
+<template>
+   <div class="row-banner-li"></div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+interface RowBannerLi {
+    theme:String
+}
+const props =  defineProps<RowBannerLi>()
+const src = computed(()=>`url(/assets/images/common/RowBanner/${props.theme}2.png)`)
+</script>
+
+<style lang="scss" scoped>
+.row-banner-li {
+    display: inline-block;
+    white-space: nowrap;
+    height: 80px;
+    width: 52px;
+    background: v-bind(src) no-repeat;
+    background-size: 52px 80px;
+    vertical-align: middle;
+}
+</style>
